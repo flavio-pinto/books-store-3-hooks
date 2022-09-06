593a9248bfeb77ec7e695c4f4f766c3dc3a4fbd1
Flavio Pinto <flaviopnt@hotmail.it>
2022-09-06 20:27:40 +0200

    SingleBook component converted to function component

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,22 +1,20 @@
 import { Component } from "react";
 import { Card } from "react-bootstrap";
 
-class SingleBook extends Component {
-  render() {
-    return (
-      <>
-        <Card
-          onClick={() => this.props.setSelectedBook(this.props.bookAsin)}
-          style={{ border: this.props.selectedBook === this.props.bookAsin ? "3px solid red" : "0px" }}
-        >
-          <Card.Img variant="top" src={this.props.imgUrl} />
-          <Card.Body>
-            <Card.Title>{this.props.title}</Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
+const SingleBook = (props) => {
+  return (
+    <>
+      <Card
+        onClick={() => props.setSelectedBook(props.bookAsin)}
+        style={{ border: props.selectedBook === props.bookAsin ? "3px solid red" : "0px" }}
+      >
+        <Card.Img variant="top" src={props.imgUrl} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default SingleBook;
